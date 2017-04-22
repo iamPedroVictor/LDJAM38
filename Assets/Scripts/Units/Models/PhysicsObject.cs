883f7c88a0917118582a00d6c36c7ebc0615a818
Pedro Victor <pedro.victor.ferreira@outlook.com.br>
2017-04-22 00:57:29 -0400

    BasicMoviment

---
+++ Assets/Scripts/Units/Models/PhysicsObject.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour {
+
+    public float minGroundNormalY = .65f;
+    [Tooltip("Modificador da gravidade")]
+    public float gravityModifier = 1f;
+
+    protected Vector2 velocity;
+    protected Vector2 targetVelocity;
+
+    protected bool grounded;
+    protected Vector2 groundNormal;
+
+    protected Rigidbody2D rb2d;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+
+    private void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+    }
+
+    protected virtual void ComputerVelocity(){
+
+
+
+    }
+
+    private void Update()
+    {
+        targetVelocity = Vector2.zero;
+        ComputerVelocity();
+    }
+
+    private void FixedUpdate()
+    {
+        //Adiciona a gravidade no movimento
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        velocity.x = targetVelocity.x;
+
+        grounded = false;
+
+        //a diferenca da posicao
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        Vector2 moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+
+        Vector2 move = moveAlongGround * deltaPosition.x;
+
+        Movement(move, false);
+
+        //da a movimentacao vertical
+        move = Vector2.up * deltaPosition.y;
+
+        Movement(move,true);
+
+    }
+
+    void Movement(Vector2 move, bool yMoviment)
+    {
+        float distance = move.magnitude;
+
+        if(distance > minMoveDistance)
+        {
+            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear();
+            for(int i = 0; i < count; i++){
+                hitBufferList.Add(hitBuffer[i]);
+            }
+
+            for(int i = 0; i < hitBufferList.Count; i++){
+                Vector2 currentNormal = hitBufferList[i].normal;
+                if(currentNormal.y > minGroundNormalY){
+                    grounded = true;
+                    if (yMoviment){
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if(projection < 0){
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+
+            }
+                        
+
+        }
+
+        //Da a nova posicao
+        rb2d.position = rb2d.position + move.normalized * distance;
+    }
+
+}
---
+++ Assets/Scripts/Units/Player/PlayerMovimentController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovimentController : PhysicsObject {
+
+    public float jumpTakeOffSpeed = 7;
+    public float maxSpeed = 7;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    protected override void ComputerVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if(Input.GetButtonDown("Jump") && grounded){
+
+            velocity.y = jumpTakeOffSpeed;
+
+        }else if(Input.GetButtonUp("Jump")){
+            if(velocity.y > 0)
+            {
+                velocity.y = velocity.y * .5f;
+            }
+        }
+
+        targetVelocity = move * maxSpeed;
+
+    }
+
+
+}
